bfeb5a03e45399e168458088690c136bbaba5cb9
Gavin Day <mngad@leeds.ac.uk>
2016-06-28 14:49:25 +0100

    iterates over top slice to find loading point

--- segment_WITHOUT_CEMENT.py
+++ segment_WITHOUT_CEMENT.py
@@ -1,108 +1,140 @@
 from scanip_api import *
+
 if InputDialog.AskYesNoQuestion('Align', 'Have you aligned the model?') == True:
-    
-    doc=App.GetDocument()
-    
-    #doc.ResampleDataByPixelSpacing(1, 1, 1, doc.PartialVolumeEffectInterpolation, doc.PartialVolumeEffectInterpolation)
-    
-    # Select vertebrae mask & open and close it
-    doc.Threshold(20, 255, Doc.CreateNewMask, doc.GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
-    doc.ApplyCloseFilter(Doc.TargetMask, 1, 1, 1, 0)
-    doc.ApplyOpenFilter(Doc.TargetMask, 1, 1, 1, 0)
-    
-    # Select both endcaps, open and close
-    doc.Threshold(10, 20, Doc.CreateNewMask, doc.GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
-    doc.ApplyCloseFilter(Doc.TargetMask, 1, 1, 1, 0)
-    doc.ApplyOpenFilter(Doc.TargetMask, 1, 1, 1, 0)
-    
-    # duplicate endcaps to allow manual flood fill to separate the two
-    doc.GetActiveMask().Duplicate()
-    
-    # Rename all masks
-   
-    doc.GetMaskByName("Mask 1").Activate()
-    doc.GetMaskByName("Mask 1").SetName("Vertebrae")
-    doc.GetMaskByName("Mask 2").Activate()
-    doc.GetMaskByName("Mask 2").SetName("Inferior_endcap")
-    doc.GetMaskByName("Copy of Mask 2").Activate()
-    doc.GetMaskByName("Copy of Mask 2").SetName("Superior_endcap")
+	
+	doc=App.GetDocument()
+	
+	#downsample to 1x1x1
+	#doc.ResampleDataByPixelSpacing(1, 1, 1, doc.PartialVolumeEffectInterpolation, doc.PartialVolumeEffectInterpolation)
+	
+	# Select vertebrae mask & open and close it
+	doc.Threshold(20, 255, Doc.CreateNewMask, doc.GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
+	doc.ApplyCloseFilter(Doc.TargetMask, 1, 1, 1, 0)
+	#doc.ApplyOpenFilter(Doc.TargetMask, 1, 1, 1, 0) #no need for this
+	
+	# Select both endcaps, open and close
+	doc.Threshold(10, 20, Doc.CreateNewMask, doc.GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
+	doc.ApplyCloseFilter(Doc.TargetMask, 1, 1, 1, 0)
+	doc.ApplyOpenFilter(Doc.TargetMask, 1, 1, 1, 0) #still need it here to remove extra "cement" around the top and bottom of the vertebra
+	
+	# duplicate endcaps to allow manual flood fill to separate the two
+	doc.GetActiveMask().Duplicate()
+	
+	# Rename all masks
+	doc.GetMaskByName("Mask 1").Activate()
+	doc.GetMaskByName("Mask 1").SetName("Vertebrae")
+	doc.GetMaskByName("Mask 2").Activate()
+	doc.GetMaskByName("Mask 2").SetName("Inferior_endcap")
+	doc.GetMaskByName("Copy of Mask 2").Activate()
+	doc.GetMaskByName("Copy of Mask 2").SetName("Superior_endcap")
   
-    
-    # Subtract masks from each other
+	# Subtract masks from each other
+	doc.GetMaskByName("Inferior_endcap").Activate()
+	doc.GetActiveMask().SubtractWith(doc.GetMaskByName("Vertebrae"), doc.GetSliceIndices (Doc.OrientationYZ), Doc.OrientationYZ)
+	doc.GetMaskByName("Superior_endcap").Activate()
+	doc.GetActiveMask().SubtractWith(doc.GetMaskByName("Vertebrae"), doc.GetSliceIndices (Doc.OrientationYZ), Doc.OrientationYZ)
+	doc.GenerateFastPreview()
+	
+	#define masks
+	mask_vert = doc.GetMaskByName("Vertebrae")
+	mask_inf = doc.GetMaskByName("Inferior_endcap")
+	mask_sup = doc.GetMaskByName("Superior_endcap")
    
-    doc.GetMaskByName("Inferior_endcap").Activate()
-    doc.GetActiveMask().SubtractWith(doc.GetMaskByName("Vertebrae"), doc.GetSliceIndices (Doc.OrientationYZ), Doc.OrientationYZ)
-    doc.GetMaskByName("Superior_endcap").Activate()
-    doc.GetActiveMask().SubtractWith(doc.GetMaskByName("Vertebrae"), doc.GetSliceIndices (Doc.OrientationYZ), Doc.OrientationYZ)
-    doc.GenerateFastPreview()
-    
-    
-    
-    mask_vert = doc.GetMaskByName("Vertebrae")
-    mask_inf = doc.GetMaskByName("Inferior_endcap")
-    mask_sup = doc.GetMaskByName("Superior_endcap")
-   
-    feModel = doc.CreateFeModel()
-    feModel.AddMask(mask_vert)
-    feModel.AddMask(mask_inf)
-    feModel.AddMask(mask_sup)
+	#create FE model and add masks
+	feModel = doc.CreateFeModel()
+	feModel.AddMask(mask_vert)
+	feModel.AddMask(mask_inf)
+	feModel.AddMask(mask_sup)
 
-    feModel.SetExportType(Model.AbaqusVolume)
-    feModel.SetUseGreyscaleValues(True)
-    feModel.SetUseSmartMaskSmoothing(True)
-    feModel.SetNumSmartMaskSmoothingIterations(20)
-    App.GetDocument().SetActiveModel(App.GetDocument().GetModelByName("Model 1"))
-    App.GetDocument().GetActiveModel().SetExportUsingAbsoluteCoordinates(False)
-    App.GetDocument().GetActiveModel().SetMeshingAlgorithmType(Model.Grid)
-    App.GetDocument().GetActiveModel().SetTargetMaxGridSize(Model.GridSize1x1)
+	#model properties
+	feModel.SetExportType(Model.AbaqusVolume)
+	feModel.SetUseGreyscaleValues(True)
+	feModel.SetUseSmartMaskSmoothing(True)
+	feModel.SetNumSmartMaskSmoothingIterations(20)
+	doc.SetActiveModel(doc.GetModelByName("Model 1"))
+	doc.GetActiveModel().SetExportUsingAbsoluteCoordinates(False)#sets local coordinate system
+	doc.GetActiveModel().SetMeshingAlgorithmType(Model.Grid)
+	doc.GetActiveModel().SetTargetMaxGridSize(Model.GridSize1x1)
+	doc.GetActiveModel().SetExportUsingAbsoluteCoordinates(False)
+	doc.GetActiveModel().SetEditAdvancedParametersManually(True)
+	doc.GetActiveModel().SetAdditionalMeshQualityImprovementQualityMetric(Model.InOutRatio)
+		
+	#Set Material Properties
+	feModel.GetMaskPart(mask_inf).SetMaterial(HomogeneousMaterial(1, 2.45, 0.3, "INFERIOR_CAP_HMG"))
+	feModel.GetMaskPart(mask_sup).SetMaterial(HomogeneousMaterial(1, 2.45, 0.3, "SUPERIOR_CAP_HMG"))
+	#feModel.GetMaskPart(mask_vert).SetMaterial() # Set GS material?
+	
+	#Add contacts between vertebra and end-caps
+	feModel.AddContactPair(feModel.GetMaskPart(mask_vert), feModel.GetMaskPart(mask_inf))
+	feModel.AddContactPair(feModel.GetMaskPart(mask_vert), feModel.GetMaskPart(mask_sup))
+	
+	#shrink wrap and crop to achieve parrallel surfaces
+	doc.ShrinkWrapData(Doc.TargetAllMasks, 5, 5, 5, 5, 0, 0)
 	
 	
-    feModel.GetMaskPart(mask_inf).SetMaterial(HomogeneousMaterial(1, 2.45, 0.3, "INFERIOR_CAP_HMG"))
-    feModel.GetMaskPart(mask_sup).SetMaterial(HomogeneousMaterial(1, 2.45, 0.3, "SUPERIOR_CAP_HMG"))
-    #feModel.GetMaskPart(mask_vert).SetMaterial() # Set GS material?
-    feModel.AddContactPair(feModel.GetMaskPart(mask_vert), feModel.GetMaskPart(mask_inf))
-    feModel.AddContactPair(feModel.GetMaskPart(mask_vert), feModel.GetMaskPart(mask_sup))
-    doc.ShrinkWrapData(Doc.TargetAllMasks, 5, 5, 5, 5, 0, 0)
-    doc.CropData(0,doc.GetSliceCount(Doc.OrientationYZ),0,doc.GetSliceCount(Doc.OrientationZX),2,(doc.GetSliceCount(Doc.OrientationXY)-4))
-    App.GetDocument().GetActiveModel().AddNodeSet(App.GetDocument().GetActiveModel().GetPartByName("Inferior_endcap"), Model.Zmin)
-    App.GetDocument().GetActiveModel().AddSurfaceContact(App.GetDocument().GetActiveModel().GetPartByName("Superior_endcap"), Model.Zmax)
+	
+	#Find Loading Point
+	firstYmin = firstXmin = 200
+	doc.GetMaskByName("Superior_endcap").Activate()
+	ymin = xmin = 0
+	voxelNumBound=120
+
+	for x in range (15, doc.GetSliceCount(Doc.OrientationYZ)-15):
+		for y in range (15, doc.GetSliceCount(Doc.OrientationZX)-15):
+			curYVal=doc.GetMaskByName("Superior_endcap").GetVoxelValue(x,y,doc.GetSliceCount(Doc.OrientationXY)-2)
+			if curYVal > voxelNumBound:
+				if y<ymin:
+					ymin=y
+					if ymin < firstYmin :
+						firstYmin =ymin
+						#App.GetInstance().ShowMessage(str(firstYmin))
+					break
+				ymin = y
+	for y in range (15, doc.GetSliceCount(Doc.OrientationZX)-15):
+		for x in range (15, doc.GetSliceCount(Doc.OrientationYZ)-15):
+			curYVal=doc.GetMaskByName("Superior_endcap").GetVoxelValue(x,y,doc.GetSliceCount(Doc.OrientationXY)-2)
+			if curYVal > voxelNumBound:
+				if x<xmin:
+					xmin=x
+					if xmin < firstXmin :
+						firstXmin =xmin
+					break
+				xmin = x
+	App.GetInstance().ShowMessage('Centre point is: ' + str(firstXmin+5) + ',' + str(firstYmin+5) +',' + str(doc.GetSliceCount(Doc.OrientationXY)))
 
 	
 	
-    
-	#doc.CropData(0, 75, 0, 77, 1, 60)
-    
-    #feModel.AddContactPair(feModel.GetMaskPart(mask_sup), feModel.Zmax)
-    #feModel.AddNodeSet(feModel.GetMaskPart(mask_inf), feModel.Zmin)
+	
+	# add node sets and surface contacts for top and bottom layer
+	doc.CropData(0,doc.GetSliceCount(Doc.OrientationYZ),0,doc.GetSliceCount(Doc.OrientationZX),2,(doc.GetSliceCount(Doc.OrientationXY)-4))
+	doc.GetActiveModel().AddNodeSet(doc.GetActiveModel().GetPartByName("Inferior_endcap"), Model.Zmin)
+	doc.GetActiveModel().AddSurfaceContact(doc.GetActiveModel().GetPartByName("Superior_endcap"), Model.Zmax)
+	
+	# remove gaps between end-caps and the vertebrae
+	doc.GetMaskByName("Vertebrae").Activate()
+	doc.GetActiveMask().UnionWith(doc.GetMaskByName("Superior_endcap"), doc.GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
+	doc.ApplyCavityFillFilter()
+	doc.GetActiveMask().SubtractWith(doc.GetMaskByName("Superior_endcap"), doc.GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
+	doc.GetMaskByName("Vertebrae").Activate()
+	doc.GetActiveMask().UnionWith(doc.GetMaskByName("Inferior_endcap"), doc.GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
+	doc.ApplyCavityFillFilter()
+	doc.GetActiveMask().SubtractWith(doc.GetMaskByName("Inferior_endcap"), doc.GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
 
-    App.GetDocument().GetMaskByName("Vertebrae").Activate()
-    App.GetDocument().ApplyCavityFillFilter()
-    App.GetDocument().GetActiveModel().SetExportUsingAbsoluteCoordinates(False)
-    App.GetDocument().GetActiveModel().SetMeshingAlgorithmType(Model.Grid)
-    App.GetDocument().GetActiveModel().SetEditAdvancedParametersManually(True)
-    App.GetDocument().GetActiveModel().SetTargetMaxGridSize(Model.GridSize1x1)
-    App.GetDocument().GetActiveModel().SetAdditionalMeshQualityImprovementQualityMetric(Model.InOutRatio)
-    
-	#remove gaps between end-caps and the vertebrae
-    App.GetDocument().GetMaskByName("Vertebrae").Activate()
-    App.GetDocument().GetActiveMask().UnionWith(App.GetDocument().GetMaskByName("Superior_endcap"), App.GetDocument().GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
-    App.GetDocument().ApplyCavityFillFilter()
-    App.GetDocument().GetActiveMask().SubtractWith(App.GetDocument().GetMaskByName("Superior_endcap"), App.GetDocument().GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
-    App.GetDocument().GetMaskByName("Vertebrae").Activate()
-    App.GetDocument().GetActiveMask().UnionWith(App.GetDocument().GetMaskByName("Inferior_endcap"), App.GetDocument().GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
-    App.GetDocument().ApplyCavityFillFilter()
-    App.GetDocument().GetActiveMask().SubtractWith(App.GetDocument().GetMaskByName("Inferior_endcap"), App.GetDocument().GetSliceIndices (Doc.OrientationXY), Doc.OrientationXY)
+	# Flood fill the two end-caps to separate them
+	doc.GetMaskByName("Superior_endcap").Activate()
+	doc.FloodFillFromActiveMask(35, 15, 54, False, Doc.Mode3D, Doc.ReplaceWithMask, doc.GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
+	doc.GetMaskByName("Inferior_endcap").Activate()
+	doc.FloodFillFromActiveMask(35, 15, 1, False, Doc.Mode3D, Doc.ReplaceWithMask, doc.GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
 
-    App.GetDocument().GetMaskByName("Superior_endcap").Activate()
-    App.GetDocument().FloodFillFromActiveMask(35, 8, 54, False, Doc.Mode3D, Doc.ReplaceWithMask, App.GetDocument().GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
-    App.GetDocument().GetMaskByName("Inferior_endcap").Activate()
-    App.GetDocument().FloodFillFromActiveMask(35, 8, 1, False, Doc.Mode3D, Doc.ReplaceWithMask, App.GetDocument().GetSliceIndices(Doc.OrientationYZ), Doc.OrientationYZ)
-
-    App.GetDocument().GetMaskByName("Vertebrae").Activate()
-    App.GetDocument().ApplyCavityFillFilter()
+	# remove void from centre of vertebra
+	doc.GetMaskByName("Vertebrae").Activate()
+	doc.ApplyCavityFillFilter()
 	
 	
-    # Messages to remind manual parts
-    App.GetInstance().ShowMessage('Separate endcaps, crop and remove spinal canal cement')
+	
+	
+	doc.GenerateFastPreview()
+	# Messages to remind manual parts
+	App.GetInstance().ShowMessage('Separate endcaps, crop and remove spinal canal cement')
 else:
-    App.GetInstance().ShowMessage('Align first')
+	App.GetInstance().ShowMessage('Align first')
